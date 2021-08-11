d526580fe311a70be4dcaadace1a69a116d7c097
jpoulin587 <jpoulin587a@gmail.com>
2021-08-11 18:36:12 -0500

    switch after start menu built

--- index.js
+++ index.js
@@ -16,12 +16,62 @@ inquirer
         type: 'list',
         name: 'startChoice',
         message: 'Welcome to the Employee tracker. Please select one of the following options',
-        choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+        choices: [
+            'view all departments', 
+            'view all roles', 
+            'view all employees', 
+            'add a department', 
+            'add a role', 
+            'add an employee', 
+            'update an employee role'
+        ]
 
 
 
-    }); //end of start menu questions
+    }) //end of start menu questions
 
-};
+    .then ((data) => {
+    console.log (data)
+    
+    switch (data.startChoice){
+        case "view all departments":
+        viewAllDepartments()
+        break;
 
+        case 'view all roles':
+        viewAllRoles()
+        break;
+
+        case 'view all employees':
+        viewAllEmployees()
+        break;
+
+        case 'add a department':
+        addDepartment()
+        break;
+
+        case 'add a role':
+        addRole()
+        break;
+
+        case 'add an employee':
+        addEmployee()
+        break;
+
+        case 'update an employee role':
+        updateEmployeeRole()
+        break;
+        
+        default:
+            break;
+    }; //end of switch
+    
+    }
+
+    )}; //end of start menu
+
+
+
+
+    
 startMenu();
