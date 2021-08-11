d9614226144ffd91a61984429d2d69ce4d4df35e
jpoulin587 <jpoulin587a@gmail.com>
2021-08-11 16:31:02 -0500

    schema built

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS emp_tracker_db;
+CREATE DATABASE emp_tracker_db;
+
+USE emp_tracker_db;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL, 
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    ON DELETE SET NULL
+);
---
+++ server.js
@@ -0,0 +1,12 @@
+//(adapt from unit 12 activity 21)
+require('dotenv');
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
